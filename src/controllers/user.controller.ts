c443c92297a8ac4481943c2756495e944feb956b
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2025-06-11 22:07:18 -0300

    Substituindo o TRY CATCH pelo Async Handler

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { getFirestore } from "firebase-admin/firestore";
 import { Request, Response, NextFunction } from "express";
 import { ValidationError } from "../errors/validation.error";
+import { NotFoundError } from "../errors/not-found.error";
 
 type User = {
     id: number,
@@ -10,69 +11,58 @@ type User = {
 
 export class UsersController {
     static async getAll(req: Request, res: Response, next: NextFunction) {
-        try {
-            const snapshot = await getFirestore().collection("users").get();
-            const users = snapshot.docs.map(doc => {
-                return {
-                    id: doc.id,
-                    ...doc.data()
-                };
-            });
-            res.json(users)
-        } catch (error) {
-            next(error)
-        }
+        const snapshot = await getFirestore().collection("users").get();
+        const users = snapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            };
+        });
+        res.json(users)
     }
 
     static async getById(req: Request, res: Response, next: NextFunction) {
-        try {
-            let userId = req.params.id;
-            const doc = await getFirestore().collection("users").doc(userId).get();
+        let userId = req.params.id;
+        const doc = await getFirestore().collection("users").doc(userId).get();
+        if (doc.exists) {
             res.send({
                 id: doc.id,
                 ...doc.data()
             });
-        } catch (error) {
-            next(error)
+        } else {
+            throw new NotFoundError("Usuário não encontrado")
         }
     }
 
     static async save(req: Request, res: Response, next: NextFunction) {
-        try {
-            let user = req.body;
-            if (!user.email || user.email?.length === 0) {
-                throw new ValidationError("Email obrigatório")
-            }
-            const userSave = await getFirestore().collection("users").add(user);
-            res.send({
-                message: `Usuário ${userSave.id} criado com sucesso.`
-            });
-        } catch (error) {
-            next(error)
+        let user = req.body;
+        if (!user.email || user.email?.length === 0) {
+            throw new ValidationError("Email obrigatório")
         }
+        const userSave = await getFirestore().collection("users").add(user);
+        res.send({
+            message: `Usuário ${userSave.id} criado com sucesso.`
+        });
     }
 
     static async update(req: Request, res: Response, next: NextFunction) {
-        try {
-            let userId = req.params.id;
-            let user = req.body as User;
-            await getFirestore().collection("users").doc(userId).set({
+        let userId = req.params.id;
+        let user = req.body as User;
+        let docRef = getFirestore().collection("users").doc(userId);
+        if ((await docRef.get()).exists) {
+            await docRef.set({
                 nome: user.nome,
                 email: user.email
-            })
-            res.send({ message: "Informações do usuário atualizado" });
-        } catch (error) {
-            next(error)
+            });
+            res.status(201).send({ message: "Informações do usuário atualizadas" });
+        } else {
+            throw new NotFoundError("Usuário não encontrado")
         }
     }
 
     static async delete(req: Request, res: Response, next: NextFunction) {
-        try {
-            let userId = req.params.id;
-            await getFirestore().collection("users").doc(userId).delete();
-            res.send({ message: "Usuário deletado" });
-        } catch (error) {
-            next(error)
-        }
+        let userId = req.params.id;
+        await getFirestore().collection("users").doc(userId).delete();
+        res.status(204).send({ message: "Usuário deletado" });
     }
 }
