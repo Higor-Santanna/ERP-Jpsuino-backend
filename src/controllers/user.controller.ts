abab3294dca63623698129c67861daa07553a5b0
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2025-06-11 21:17:21 -0300

    Adicionado o tratamento de erros dentro do usuário.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { getFirestore } from "firebase-admin/firestore";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
+import { ValidationError } from "../errors/validation.error";
 
 type User = {
     id: number,
@@ -8,47 +9,70 @@ type User = {
 }
 
 export class UsersController {
-    static async getAll (req: Request, res: Response){
-        const snapshot = await getFirestore().collection("users").get();
-        const users = snapshot.docs.map(doc => {
-            return {
-                id: doc.id,
-                ...doc.data()
-            };
-        });
-        res.json(users)
+    static async getAll(req: Request, res: Response, next: NextFunction) {
+        try {
+            const snapshot = await getFirestore().collection("users").get();
+            const users = snapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data()
+                };
+            });
+            res.json(users)
+        } catch (error) {
+            next(error)
+        }
     }
 
-    static async getById(req: Request, res: Response){
-        let userId = req.params.id;
-        const doc = await getFirestore().collection("users").doc(userId).get();
-        res.send({
+    static async getById(req: Request, res: Response, next: NextFunction) {
+        try {
+            let userId = req.params.id;
+            const doc = await getFirestore().collection("users").doc(userId).get();
+            res.send({
                 id: doc.id,
                 ...doc.data()
             });
+        } catch (error) {
+            next(error)
+        }
     }
 
-    static async save(req: Request, res: Response){
-        let user = req.body;
-        const userSave = await getFirestore().collection("users").add(user);
-        res.send({
-            message: `Usuário ${userSave.id} criado com sucesso.`
-        });
+    static async save(req: Request, res: Response, next: NextFunction) {
+        try {
+            let user = req.body;
+            if (!user.email || user.email?.length === 0) {
+                throw new ValidationError("Email obrigatório")
+            }
+            const userSave = await getFirestore().collection("users").add(user);
+            res.send({
+                message: `Usuário ${userSave.id} criado com sucesso.`
+            });
+        } catch (error) {
+            next(error)
+        }
     }
 
-    static async update(req:Request, res: Response){
-        let userId = req.params.id;
-        let user = req.body as User;
-        await getFirestore().collection("users").doc(userId).set({
-            nome: user.nome,
-            email : user.email
-        })
-        res.send({ message: "Informações do usuário atualizado"});
+    static async update(req: Request, res: Response, next: NextFunction) {
+        try {
+            let userId = req.params.id;
+            let user = req.body as User;
+            await getFirestore().collection("users").doc(userId).set({
+                nome: user.nome,
+                email: user.email
+            })
+            res.send({ message: "Informações do usuário atualizado" });
+        } catch (error) {
+            next(error)
+        }
     }
 
-    static async delete(req: Request, res: Response){
-        let userId = req.params.id;
-        await getFirestore().collection("users").doc(userId).delete();
-        res.send({ message: "Usuário deletado"});
+    static async delete(req: Request, res: Response, next: NextFunction) {
+        try {
+            let userId = req.params.id;
+            await getFirestore().collection("users").doc(userId).delete();
+            res.send({ message: "Usuário deletado" });
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { initializeApp } from 'firebase-admin/app';
 import { routes } from "./routes";
+import { erroHandler } from "./middlewares/error-handler.middeware";
 
-initializeApp();
 const app = express();
 const port = 3000;
 
+initializeApp();
 routes(app)
+erroHandler(app);
 
 app.listen(port, () => {
     console.log(`Servidor funcionando na porta ${port}`);
